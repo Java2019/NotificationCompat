3b1e3a4c35a8ae3e1d6cec9be7c3db895372b94f
java2019 <java_2019@mail.ru>
2017-07-10 18:00:43 +0300

    first init

--- app/src/main/java/com/simples/res/notificationcompat/NotificationView.java
+++ app/src/main/java/com/simples/res/notificationcompat/NotificationView.java
@@ -1,7 +1,15 @@
 package com.simples.res.notificationcompat;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by use on 10.07.17.
  */
-public class NotificationView {
+public class NotificationView extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notification);
+    }
 }
