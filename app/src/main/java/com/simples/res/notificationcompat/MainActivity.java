1d4e4acd091920ca549a58132b8a0bae9116a7b7
java2019 <java_2019@mail.ru>
2017-07-10 17:41:55 +0300

    first init

--- app/src/main/java/com/simples/res/notificationcompat/MainActivity.java
+++ app/src/main/java/com/simples/res/notificationcompat/MainActivity.java
@@ -1,13 +1,56 @@
 package com.simples.res.notificationcompat;
 
+import android.app.NotificationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NotificationManager manager;
+    private int notificationID = 100;
+    private int numMessages = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button startBtn = (Button)findViewById(R.id.bStart);
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                displayNotification();
+            }
+        });
+
+        Button cancelBtn = (Button)findViewById(R.id.bCancel);
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cancelNotification();
+            }
+        });
+
+        Button updateBtn = (Button)findViewById(R.id.update);
+        updateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateNotification();
+            }
+        });
+    }
+
+    protected void displayNotification(){
+
+    }
+
+    protected void cancelNotification(){
+
+    }
+
+    protected void updateNotification(){
+
     }
 }
